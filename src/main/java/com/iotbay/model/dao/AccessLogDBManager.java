444843bfec929d68ac1c70164d9eb7595fdfbf8d
MaximumTab <maximumtab@outlook.com>
2025-05-06 17:32:46 +1000

    Set up DBManager, DAO and DBConnector

--- src/main/java/com/iotbay/model/dao/AccessLogDBManager.java
+++ src/main/java/com/iotbay/model/dao/AccessLogDBManager.java
@@ -1,4 +1,14 @@
 package com.iotbay.model.dao;
 
-public class AccessLogDBManager {
+import com.iotbay.model.AccessLog;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class AccessLogDBManager extends DBManager<AccessLog>
+{
+    public AccessLogDBManager(Connection connection) throws SQLException
+    {
+        super(connection);
+    }
 }
--- src/main/java/com/iotbay/model/dao/DevicesDBManager.java
+++ src/main/java/com/iotbay/model/dao/DevicesDBManager.java
@@ -1,4 +1,20 @@
 package com.iotbay.model.dao;
 
-public class DevicesDBManager {
+import com.iotbay.model.Devices;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DevicesDBManager extends DBManager<Devices>
+{
+    public DevicesDBManager(Connection connection) throws SQLException
+    {
+        super(connection);
+    }
+
+    public Devices add(Devices device) throws SQLException
+    {
+        PreparedStatement ps = connection.prepareStatement("");
+    }
 }
