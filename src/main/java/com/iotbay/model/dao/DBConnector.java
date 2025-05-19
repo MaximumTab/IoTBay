bdabaab6ba80e21bd90dd960827712c569f1e0e6
MaximumTab <maximumtab@outlook.com>
2025-05-19 19:46:40 +1000

    fixed

--- src/main/java/com/iotbay/model/dao/DBConnector.java
+++ src/main/java/com/iotbay/model/dao/DBConnector.java
@@ -15,7 +15,7 @@ public class DBConnector {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-        String url = "jdbc:sqlite:C:/Users/paken/.SmartTomcat/IoTBay/IOTBayWebsite/IotBay.db";
+        String url = "jdbc:sqlite:IOTBay.db";
         try {
             connection = DriverManager.getConnection(url);
             connection.setAutoCommit(true);
